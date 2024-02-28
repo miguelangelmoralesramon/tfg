935fcab8658b788e8a0d105050794c4cc3e6c695
miguelangelmoralesramon <miguelangelmoralesramon@gmail.com>
2024-02-28 22:47:14 +0100

    Added encoder layer, sequential encoder and encoder

--- transformers.py
+++ transformers.py
@@ -126,3 +126,59 @@ class PositionFeedForward(nn.Module):
         x = self.dropout(x)
         x = self.linear2(x)
         return x
+
+class EncoderLayer(nn.Module):
+    def __init__(self,d_model,ffn_hidden,num_heads,drop_prob):
+        super(EncoderLayer,self).__init__()
+        self.attention = MultiHeadAttention
+        self.norm1 = LayerNormalization(param_shape=[d_model])
+        self.dropout1 = nn.Dropout(p=drop_prob)
+        self.ffn = PositionFeedForward(d_model=d_model,hidden=ffn_hidden,drop_prob=drop_prob)
+        self.norm2 = LayerNormalization(param_shape=[d_model])
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    def forward(self,x,self_attention_mask):
+        residual_x = x.clone()
+        x = self.attention(x,mask=self_attention_mask)
+        x = self.dropout1(x)
+        x = self.norm1(x+residual_x)
+        residual_x = x.clone()
+        x = self.ffn(x)
+        x = self.dropout2(x)
+        x = self.norm2(x+residual_x)
+        return x 
+    
+class SequentialEncoder(nn.Sequential):
+    def forward(self,*inputs):
+        x,self_attention_mask = inputs
+        for module in self._modules.values():
+            x = module(x,self_attention_mask)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self,
+                 d_model,
+                 ffn_hidden,
+                 num_heads,
+                 drop_prob,
+                 num_layers.
+                 seq_length,
+                 lang_to_ind,
+                 START_TOKEN,
+                 END_TOKEN,
+                 PADDING_TOKEN):
+        super().__init__()
+        self.sentence_embedding = SentenceEmbedding(seq_length, d_model, lang_to_ind, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.layers = SequentialEncoder(*[EncoderLayer(d_model, ffn_hidden, num_heads, drop_prob)
+                                      for _ in range(num_layers)])
+        
+    def forward(self,x,self_attention_mask,start_token,end_token):
+        x = self.sentence_embedding(x,start_token,end_token)
+        x = self.layers(x,self_attention_mask)
+        return x
+    
+class MultiheadCrossAttention(nn.Module):
+    def __init__(self,d_model,num_heads):
+        super().__init__()
+        
+        
